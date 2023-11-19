42ea3640c9d65893a817c2247212de66ce5655c2
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2023-11-19 20:34:12 +0300

    updated

--- app/NumberGuessingGame/js/app.js
+++ app/NumberGuessingGame/js/app.js
@@ -1,27 +1,3 @@
-// ALGORİTMA
-
-/*
-1. random ile rastgele bir sayı üret ve bunu randomNumber ile tut.
-2. score başlangıç değeri 10 ve highScore başlangıç değeri 0 olsun.
-3. infonun içeriğini değiştirecek fonksiyonu yaz.
-4. kontrol et butonu için bir event yaz. bu fonksiyon inputtaki
-veriyi secretNumber ile eşit mi kontrol etsin.
-- input boş girilebilir kontrol et
-- sayılar eşitse yani doğru tahminse bg yeşil olsun
-ve burada score kontrolleri yap
-- sayılar eşit değilse:
-infonun değişmesini kontrol et.
-
-5. tekrar başlat butonu için event yaz.
-- tekrardan 0-20 arası sayı üretsin
-- score başlangıç değerine dönsün
-- info değişsin
-- secretNumber ? olmalı
-- input boş olmalı
-
-*/
-
-
 var randomNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(randomNumber);
 
