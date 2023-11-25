801d0aa0916eb7b4f922e8c294d3016ac6ebd703
Beyzanur Tekinli <beyza.tekinli.bt@gmail.com>
2023-11-25 18:50:27 +0300

    updated

--- app/TodoApp/app.js
+++ app/TodoApp/app.js
@@ -1,9 +1,17 @@
 var input = document.querySelector('#add');
 var btn = document.querySelector('#btn');
 var list = document.querySelector('#list');
+var del = document.querySelectorAll('.delete');
 
 btn.addEventListener('click', btnClick);
 list.addEventListener('click', listClick);
+
+del.forEach(function(button) {
+    button.addEventListener('click', function() {
+        var listItem = this.parentNode;
+        listItem.parentNode.removeChild(listItem);
+    });
+});
 
 function btnClick() {
     var txt = input.value;
@@ -15,7 +23,19 @@ function btnClick() {
         li = document.createElement("li");
         li.classList.add("element-group__list__item");
         li.innerHTML = txt;
-        list.insertBefore(li, list.childNodes[0]);
+
+        var delBtn = document.createElement("button");
+        delBtn.classList.add("delete");
+        delBtn.textContent = "Delete";
+
+        delBtn.addEventListener('click', function() {
+            var listItem = this.parentNode;
+            listItem.parentNode.removeChild(listItem);
+        });
+
+        li.appendChild(delBtn);
+        list.insertBefore(li, list.firstChild);
+        
         input.value = "";
     }
 }
